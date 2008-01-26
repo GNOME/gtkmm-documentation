8e2bfcec769c1e70704a5669f13699fbf409ee49
Murray Cumming <murrayc@murrayc.com>
2008-01-26 10:03:25 +0000

    Get the volumes for the drives too, though no drives are reported for some
    2008-01-26  Murray Cumming  <murrayc@murrayc.com>
    
    * examples/book/giomm/volumes/main.cc: Get the volumes for the drives 
    too, though no drives are reported for some reason.
    
    svn path=/trunk/; revision=30

--- examples/book/giomm/volumes/main.cc
+++ examples/book/giomm/volumes/main.cc
@@ -11,6 +11,7 @@ int main(int argc, char** argv)
     if(!volume_monitor)
       std::cerr << "Gio::VolumeMonitor::get() failed." << std::endl;
 
+    //Get the drives:
     typedef std::list< Glib::RefPtr<Gio::Drive> > type_list_drives;
     type_list_drives list_drives = volume_monitor->get_connected_drives();
 
@@ -20,9 +21,17 @@ int main(int argc, char** argv)
     for(type_list_drives::iterator iter = list_drives.begin(); iter != list_drives.end(); ++iter)
     {
       Glib::RefPtr<Gio::Drive> drive = *iter;
-      std::cout << "drive: " << drive->get_name() << std::endl;
+      std::cout << "Drive: " << drive->get_name() << std::endl;
 
-      //TODO: get the volumes in the drive.
+      //Get the volumes in the drive:
+      typedef std::list< Glib::RefPtr<Gio::Volume> > type_list_volumes;
+      type_list_volumes list_volumes = volume_monitor->get_volumes();
+
+      for(type_list_volumes::iterator iter = list_volumes.begin(); iter != list_volumes.end(); ++iter)
+      {
+        Glib::RefPtr<Gio::Volume> volume = *iter;
+        std::cout << "  Volume: " << volume->get_name() << std::endl;
+      }
     }
   }
   catch(const Glib::Exception& ex)
