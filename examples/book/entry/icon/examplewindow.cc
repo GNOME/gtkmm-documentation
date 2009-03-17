4f28df662e42969f1d2194a378a62f278ecff584
Murray Cumming <murrayc@murrayc.com>
2009-03-17 12:17:38 +0000

    2.16.0:
    2009-03-17  Murray Cumming  <murrayc@murrayc.com>
    
    * examples/book/entry/Makefile.am:
    * examples/book/entry/icon/:
    * examples/book/entry/progress/: Added entry/progress and entry/icon 
    examples, just to test that API.
    
    svn path=/trunk/; revision=84

---
+++ examples/book/entry/icon/examplewindow.cc
@@ -0,0 +1,59 @@
+//$Id: examplewindow.cc 836 2007-05-09 03:02:38Z jjongsma $ -*- c++ -*-
+
+/* gtkmm example Copyright (C) 2002 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "examplewindow.h"
+#include <iostream>
+
+ExampleWindow::ExampleWindow()
+: m_Button_Close("Close")
+{
+  set_title("Gtk::Entry");
+
+  add(m_VBox);
+
+  m_Entry.set_max_length(50);
+  m_Entry.set_text("Hello world");
+  m_VBox.pack_start(m_Entry, Gtk::PACK_SHRINK);
+
+  m_Entry.set_icon_from_stock(Gtk::Stock::FIND);
+  m_Entry.signal_icon_press().connect( sigc::mem_fun(*this, &ExampleWindow::on_icon_pressed) );
+ 
+
+  m_Button_Close.signal_clicked().connect( sigc::mem_fun(*this,
+              &ExampleWindow::on_button_close) );
+  m_VBox.pack_start(m_Button_Close, Gtk::PACK_SHRINK);
+  m_Button_Close.set_flags(Gtk::CAN_DEFAULT);
+  m_Button_Close.grab_default();
+
+  show_all_children();
+}
+
+ExampleWindow::~ExampleWindow()
+{
+}
+
+void ExampleWindow::on_icon_pressed(Gtk::EntryIconPosition /* icon_pos */, const GdkEventButton* /* event */)
+{
+  std::cout << "Icon pressed." << std::endl;
+}
+
+void ExampleWindow::on_button_close()
+{
+  hide();
+}
+
---
+++ examples/book/entry/progress/examplewindow.cc
@@ -0,0 +1,68 @@
+//$Id: examplewindow.cc 836 2007-05-09 03:02:38Z jjongsma $ -*- c++ -*-
+
+/* gtkmm example Copyright (C) 2002 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "examplewindow.h"
+#include <iostream>
+
+ExampleWindow::ExampleWindow()
+: m_Button_Close("Close")
+{
+  set_title("Gtk::Entry");
+
+  add(m_VBox);
+
+  m_Entry.set_max_length(50);
+  m_Entry.set_text("Hello world");
+  m_VBox.pack_start(m_Entry, Gtk::PACK_SHRINK);
+
+  //Change the progress fraction every 0.1 second:
+  Glib::signal_timeout().connect(
+    sigc::mem_fun(*this, &ExampleWindow::on_timeout), 
+    100
+  );
+
+  m_Button_Close.signal_clicked().connect( sigc::mem_fun(*this,
+              &ExampleWindow::on_button_close) );
+  m_VBox.pack_start(m_Button_Close, Gtk::PACK_SHRINK);
+  m_Button_Close.set_flags(Gtk::CAN_DEFAULT);
+  m_Button_Close.grab_default();
+
+  show_all_children();
+}
+
+ExampleWindow::~ExampleWindow()
+{
+}
+
+bool ExampleWindow::on_timeout()
+{
+  static double fraction = 0;
+  m_Entry.set_progress_fraction(fraction);
+
+  fraction += 0.01;
+  if(fraction > 1)
+    fraction = 0;
+
+  return true;
+}
+
+void ExampleWindow::on_button_close()
+{
+  hide();
+}
+
