d1602093ea1f2b9dc23c0d45c855a94bba4b9d43
Daniel Elstner <daniel@src.gnome.org>
2009-03-23 16:08:51 +0000

    Fix --disable-api-exceptions build
    * examples/book/builder/basic/main.cc: Conditionalize all exception
    handling in order to fix the build with --disable-api-exceptions.
    * examples/book/builder/derived/main.cc: ditto,
    * examples/book/custom/custom_widget/mywidget.{cc,h}: ditto,
    * examples/book/printing/advanced/examplewindow.cc: ditto,
    * examples/book/printing/advanced/previewdialog.cc: ditto,
    * examples/book/printing/simple/examplewindow.cc: ditto,
    * examples/book/tooltips/examplewindow.cc: ditto,
    * examples/book/treeview/popup/treeview_withpopup.cc: ditto,
    
    svn path=/trunk/; revision=85

--- examples/book/builder/basic/main.cc
+++ examples/book/builder/basic/main.cc
@@ -3,6 +3,7 @@
 
 Gtk::Dialog* pDialog = 0;
 
+static
 void on_button_clicked()
 {
   if(pDialog)
@@ -15,6 +16,7 @@ int main (int argc, char **argv)
 
   //Load the GtkBuilder file and instantiate its widgets:
   Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+#ifdef GLIBMM_EXCEPTIONS_ENABLED
   try
   {
     refBuilder->add_from_file("basic.ui");
@@ -29,6 +31,15 @@ int main (int argc, char **argv)
     std::cerr << "BuilderError: " << ex.what() << std::endl;
     return 1;
   }
+#else
+  std::auto_ptr<Glib::Error> error;
+
+  if (!refBuilder->add_from_file("basic.ui", error))
+  {
+    std::cerr << error->what() << std::endl;
+    return 1;
+  }
+#endif /* !GLIBMM_EXCEPTIONS_ENABLED */
 
   //Get the GtkBuilder-instantiated Dialog:
   refBuilder->get_widget("DialogBasic", pDialog);
@@ -47,4 +58,3 @@ int main (int argc, char **argv)
 
   return 0;
 }
-
