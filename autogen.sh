346f66aa550d7d6f12b5b214b968efa00214ffc7
Daniel Elstner <danielk@openismus.com>
2009-09-14 16:24:07 +0200

    Enable verbose output of autoreconf
    * autogen.sh: Pass the --verbose option to autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,6 @@ test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
   gnome-doc-prepare --automake --copy --force &&
-  autoreconf --force --install
+  autoreconf --force --install --verbose
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
