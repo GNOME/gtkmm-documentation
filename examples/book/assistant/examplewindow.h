2e68da2fd2a96f32d7404036377f778e9cc907ce
David King <davidk@openismus.com>
2010-04-01 17:07:16 +0200

    Add Gtk::Assistant example and tutorial
    * docs/tutorial/Makefile.am:
    * docs/tutorial/C/figures/assistant.png: Add screenshot.
    * docs/tutorial/C/gtkmm-tutorial-in.xml: Add tutorial text.
    * examples/book/assistant/*:
    * examples/Makefile.am: Add example source code.

---
+++ examples/book/assistant/examplewindow.h
@@ -0,0 +1,46 @@
+/* gtkmm example Copyright (C) 2010 Openismus GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GTKMM_EXAMPLEWINDOW_H
+#define GTKMM_EXAMPLEWINDOW_H
+
+#include <gtkmm.h>
+
+class ExampleWindow : public Gtk::Assistant
+{
+public:
+  ExampleWindow();
+  virtual ~ExampleWindow();
+
+private:
+  // Signal handlers:
+  void on_assistant_apply();
+  void on_assistant_cancel();
+  void on_assistant_close();
+  void on_assistant_prepare(Gtk::Widget* widget);
+  void on_entry_changed();
+
+  // Member functions:
+  void print_status();
+
+  // Child widgets:
+  Gtk::HBox m_box;
+  Gtk::Label m_label1, m_label2;
+  Gtk::CheckButton m_check;
+  Gtk::Entry m_entry;
+};
+
+#endif /* GTKMM_EXAMPLEWINDOW_H */
