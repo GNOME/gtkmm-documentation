b52df6cf2cb638475511ae9b6b3b5346fcf7a22c
Murray Cumming <murrayc@murrayc.com>
2011-01-19 09:21:21 +0100

    Indenting correction.

--- examples/book/base/base.cc
+++ examples/book/base/base.cc
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-    Gtk::Main kit(argc, argv);
+  Gtk::Main kit(argc, argv);
 
-    Gtk::Window window;
+  Gtk::Window window;
 
-    Gtk::Main::run(window);
+  Gtk::Main::run(window);
     
-    return 0;
+  return 0;
 }
