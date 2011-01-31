e32c327a1c2859ea32a7c37eeada179481723c25
Kjell Ahlstedt <kjell.ahlstedt@bredband.net>
2011-01-31 15:16:54 +0100

    Build: Check for mm-common.
      * autogen.sh: Add test that mm-common is installed.
      This fixes bug #628713 (Patricia Santana Cruz)

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,9 @@
 #! /bin/sh -e
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
+ # gtkmm-documentation depends on the mm-common module. Stop if it's not installed.
+mm-common-prepare --version
+
 (
   cd "$srcdir" &&
   gnome-doc-prepare --automake --copy --force &&
