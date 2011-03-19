823a50c5557f2eddcf514ff8579ddb4c12565300
Murray Cumming <murrayc@murrayc.com>
2011-03-19 13:26:39 +0100

    Split the Gtk::Application example into two.
    * examples/book/application/: Split this into:
    * examples/book/application/simple/ with no command-line handling, and:
    * examples/book/application/command_line_handling/ because custom
    handling of the command line makes the open signal (and maybe others) useless.
    As planned on bug #637445.

---
+++ examples/book/application/simple/exampleapplication.cc
@@ -0,0 +1,89 @@
+/* gtkmm example Copyright (C) 2002 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "exampleapplication.h"
+#include "examplewindow.h"
+#include <iostream>
+
+ExampleApplication::ExampleApplication()
+: Gtk::Application("org.gtkmm.examples.application", Gio::APPLICATION_HANDLES_OPEN)
+{
+}
+
+Glib::RefPtr<ExampleApplication> ExampleApplication::create()
+{
+  return Glib::RefPtr<ExampleApplication>( new ExampleApplication() );
+}
+
+void ExampleApplication::create_window(const Glib::RefPtr<Gio::File>& file)
+{
+  ExampleWindow* window = new ExampleWindow();
+
+  //Make sure that the application runs for as long this window is still open:
+  add_window(*window);
+
+  //Delete the window when it is hidden.
+  //That's enough for this simple example.
+  window->signal_hide().connect(sigc::bind<Gtk::Window*>(sigc::mem_fun(*this,
+    &ExampleApplication::on_window_hide), window));
+
+  window->show();
+
+  if(!file)
+  {
+    //This is probably an new empty file, as a result of an activation rather
+    //than an open.
+    return;
+  }
+
+  const bool loaded = window->load_file(file);
+  if(!loaded)
+    std::cerr << "This file could not be loaded: " << file->get_path() << std::endl;
+}
+
+void ExampleApplication::on_window_hide(Gtk::Window* window)
+{
+  delete window;
+}
+
+void ExampleApplication::on_activate()
+{
+  //std::cout << "debug1: " << G_STRFUNC << std::endl;
+  // The application has been started, so let's show a window.
+  // A real application might want to reuse this "empty" window in on_open(),
+  // when asked to open a file, if no changes have been made yet.
+  create_window();
+}
+
+void ExampleApplication::on_open(const Gio::Application::type_vec_files& files,
+  const Glib::ustring& hint)
+{
+  // The application has been asked to open some files,
+  // so let's open a new window for each one.
+  //std::cout << "debug: files.size()=" << files.size() << std::endl;
+  for(guint i = 0; i < files.size(); i++)
+  {
+    Glib::RefPtr<Gio::File> file = files[0];
+    if(!file)
+    {
+      std::cerr << G_STRFUNC << ": file is null." << std::endl;
+    }
+    else
+      create_window(file);
+  }
+
+  Application::on_open(files, hint);
+}
---
+++ examples/book/application/simple/examplewindow.cc
@@ -0,0 +1,66 @@
+/* gtkmm example Copyright (C) 2002 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <iostream>
+
+#include "examplewindow.h"
+
+ExampleWindow::ExampleWindow()
+{
+  set_title("Gio::Application example");
+
+  add(m_scrolledwindow);
+  m_scrolledwindow.add(m_view);
+}
+
+bool ExampleWindow::load_file(const Glib::RefPtr<Gio::File>& file)
+{    
+  if(!file)
+    return false;
+
+  try
+  {
+    char* contents = 0;
+    gsize length = 0;
+
+    if(file->load_contents(contents, length))
+    {
+      if(contents && length)
+      {
+        const Glib::ustring text(contents);
+        Glib::RefPtr<Gtk::TextBuffer> buffer = m_view.get_buffer();
+        buffer->set_text(text);
+      }
+      g_free(contents);
+    }
+  }
+  catch (const Glib::Error& ex)
+  {
+    std::cerr << G_STRFUNC << ": exception while opening file: " << file->get_uri() <<  std::endl <<
+      "  exception: " << ex.what() << std::endl;
+    
+    //Tell the application that this window can no longer be useful to 
+    //this application, so it can forget about it. The instance might then exit 
+    //if this is its last open window.
+    //Note that we must be careful that the caller only calls this method _after_ 
+    //calling show(), or this would be useless: 
+    hide();
+    return false;
+  }
+
+  show_all_children();
+  return true;
+}
