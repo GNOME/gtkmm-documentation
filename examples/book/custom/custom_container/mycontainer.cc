6050bfa7ccc61d7205b2d6e41f93db2ce39ba746
Kjell Ahlstedt <kjell.ahlstedt@bredband.net>
2013-03-03 19:01:06 +0100

    Add some info to the custom/custom_container example.
    * examples/book/custom/custom_container/mycontainer.cc: Add some comments and
    commented-out code to the destructor. The code can be necessary, but it
    should not be activated unless a version of gtkmm is used where bug 605728
    has been fixed.

--- examples/book/custom/custom_container/mycontainer.cc
+++ examples/book/custom/custom_container/mycontainer.cc
@@ -29,6 +29,19 @@ MyContainer::MyContainer()
 
 MyContainer::~MyContainer()
 {
+/*
+  // These calls to Gtk::Widget::unparent() are necessary if MyContainer is
+  // deleted before its children. But if you use a version of gtkmm where bug
+  // https://bugzilla.gnome.org/show_bug.cgi?id=605728
+  // has not been fixed (gtkmm 3.7.10 or earlier) and the children are deleted
+  // before the container, these calls can make the program crash.
+  // That's because on_remove() is not called, when the children are deleted.
+  if (m_child_one)
+    m_child_one->unparent();
+
+  if (m_child_two)
+    m_child_two->unparent();
+*/
 }
 
 void MyContainer::set_child_widgets(Gtk::Widget& child_one,
