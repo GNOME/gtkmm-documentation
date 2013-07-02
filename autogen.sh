4fd65bbb99525fac71ebc92e8f65ecf3ecbe2728
José Alburquerque <jaalburquerque@gmail.com>
2013-07-02 17:38:06 -0400

    Auto-generate the ChangeLog from the git log for 'make dist'.
    	* autogen.sh: Call mm-common-prepare with the --copy and --force flags
    	so that the additional dist-changelog.am file from mm-common (among
    	others) can be copied to the build/ directory so that it is possible
    	to include that file so that auto-generation of the ChangeLog from the
    	git commit messages is possible on 'make dist'.
    	* Makefile.am: Include the mentioned .am file so that the ChangeLog is
    	automatically generated from the git commit messages on 'make dist'.
    	* configure.ac: Check for 'sed' which is used for the ChangeLog
    	generation from the git commit messages on 'make dist'.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,13 @@
 #! /bin/sh -e
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
- # gtkmm-documentation depends on the mm-common module. Stop if it's not installed.
-mm-common-prepare --version
+
+# gtkmm-documentation depends on the mm-common module. Stop if it's not
+# installed.
+# Also copy files from mm-common to the build/ directory so that things like
+# auto-generating the ChangeLog from the git commit messages on 'make dist' is
+# possible.
+mm-common-prepare --copy --force "$srcdir"
 
 (
   cd "$srcdir" &&
