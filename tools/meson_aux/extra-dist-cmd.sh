4c303e6cc7bf19ffbb21cbe90ee201b36937bfcb
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2019-04-30 15:50:22 +0200

    Update the meson.build files and the auxiliary scripts
    meson+ninja can now validate XML files and build a PDF file.

--- tools/meson_aux/extra-dist-cmd.sh
+++ tools/meson_aux/extra-dist-cmd.sh
@@ -2,16 +2,34 @@
 
 # External command, intended to be called with add_dist_script() in meson.build
 
-# extra-dist-cmd.sh <root_build_dir> <relative_dist_dir> <tutorial_languages>
+# extra-dist-cmd.sh <root_source_dir> <root_build_dir> <relative_dist_dir> <tutorial_languages>
 
 # relative_dist_dir is the distribution directory path relative to root_build_dir.
+# Meson does not preserve timestamps on distributed files. Neither does this script.
+
+# Make a ChangeLog file for distribution.
+git --git-dir="$1/.git" --work-tree="$1" log --no-merges --date=short --max-count=200 \
+    --pretty='tformat:%cd  %an  <%ae>%n%n  %s%n%w(0,0,2)%+b' > "$2/$3/ChangeLog"
 
 # Distribute some built files in addition to the files in the local git clone.
-cd "$1"
-# English index.docbook and html files
-cp "docs/tutorial/index.docbook" "$2/docs/tutorial/C/"
-cp --recursive "docs/tutorial/html/" "$2/docs/tutorial/"
+cd "$2"
+dist_docs_tutorial="$3/docs/tutorial"
+
+# English index.docbook and html files.
+cp "docs/tutorial/index.docbook" "$dist_docs_tutorial/C/"
+cp --recursive "docs/tutorial/html/" "$dist_docs_tutorial/"
+
 # .mo files with translations and translated index.docbook files.
-for lang in $3; do
-  cp "docs/tutorial/$lang/$lang.mo" "docs/tutorial/$lang/index.docbook" "$2/docs/tutorial/$lang/"
+for lang in $4; do
+  cp "docs/tutorial/$lang/$lang.mo" "docs/tutorial/$lang/index.docbook" "$dist_docs_tutorial/$lang/"
 done
+
+# If there is an updated PDF file, include it in the tarball.
+pdf_file="docs/tutorial/programming-with-gtkmm.pdf"
+if [ -f "$pdf_file" -a "$pdf_file" -nt "docs/tutorial/index.docbook" ]; then
+  cp "$pdf_file" "$dist_docs_tutorial/C/"
+fi
+
+# Remove all .gitignore files and an empty $3/build directory.
+find "$3" -name ".gitignore" -exec rm '{}' \;
+rmdir --ignore-fail-on-non-empty "$3/build"
--- tools/meson_aux/tutorial-custom-cmd.sh
+++ tools/meson_aux/tutorial-custom-cmd.sh
@@ -2,18 +2,42 @@
 
 # External command, intended to be called with custom_target() in meson.build
 
-if [ "x$1" = "xpo2mo" ]; then
+case $1 in
+po2mo)
   # tutorial-custom-cmd.sh po2mo <relative_dir_name> <build_dir> <input_po_file> <output_mo_file> <stamp_file_path>
   ( cd "$3"; mkdir --parents "$2" )
   msgfmt -o "$5" "$4"
   touch "$6"
-elif [ "x$1" = "xdocbook" ]; then
+  ;;
+docbook)
   # tutorial-custom-cmd.sh docbook <input_mo_file> <input_xml_file> <output_xml_dir> <stamp_file_path>
+  # Create XML file with translation.
   itstool -m "$2" -o "$4" "$3"
   touch "$5"
-elif [ "x$1" = "xxmllint" ]; then
-  # tutorial-custom-cmd.sh xmllint <input_xml_file> <stamp_file_path>
-  xmllint --nonet --noout --noent --path "$(dirname "$2")" --xinclude "$2"
-  touch "$3"
-fi
+  ;;
+xmllint)
+  # tutorial-custom-cmd.sh xmllint <validate> <input_xml_file> <stamp_file_path>
+  validate=""
+  if [ "x$2" = "xtrue" ]; then
+    validate="--postvalid"
+  fi
+  xmllint --noout --noent --xinclude $validate "$3"
+  touch "$4"
+  ;;
+pdf)
+  # tutorial-custom-cmd.sh pdf <input_xml_file> <figures_dir> <output_pdf_file>
+  output_dir="$(dirname "$4")"
+  output_basename="$(basename -s .pdf "$4")"
+  xml_file="$output_dir/$output_basename.xml"
+
+  # We need to produce a full examples XML with all of the XIncludes done.
+	xmllint --xinclude --postvalid --output "$xml_file" "$2"
+
+	# We also need to copy the figures from the source directory, so they
+	# can be found from the XML files.
+	cp --preserve --recursive "$3" "$output_dir/$(basename "$3")"
+
+	docbook2pdf --output "$output_dir" "$xml_file"
+	;;
+esac
 
