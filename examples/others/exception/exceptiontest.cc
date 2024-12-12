313deb9bd3375ed320cb5fbc69a78584b59033fe
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2024-12-12 13:29:45 +0100

    examples/others/exception: Use Gtk::make_managed()
    and make the conn_handler1 and conn_handler2 connections member data,
    so they still exist when they are used in signal handlers.

--- examples/others/exception/exceptiontest.cc
+++ examples/others/exception/exceptiontest.cc
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <gtkmm/box.h>
 #include <glibmm/convert.h>
@@ -7,10 +6,8 @@
 #include <gtkmm/window.h>
 #include <gtkmm/button.h>
 
-
 namespace
 {
-
 class MyButton : public Gtk::Button
 {
 public:
@@ -23,7 +20,6 @@ protected:
 class MyException1 {};
 class MyException2 {};
 class MyException3 {};
-
 
 MyButton::MyButton(const Glib::ustring& label)
 :
@@ -45,7 +41,7 @@ void global_on_clicked()
 void global_on_clicked_throw_glib_error()
 {
   std::cout << "The Button was clicked.\n";
-  Glib::locale_from_utf8("Und tschüss!"); // invalid UTF-8
+  (void)Glib::locale_from_utf8("Und tsch\xfcss!"); // invalid UTF-8
 }
 
 void global_on_clicked_throw_std_exception()
@@ -89,34 +85,35 @@ class MyWindow : public Gtk::Window
 {
 public:
   MyWindow();
+private:
+  sigc::connection conn_handler1;
+  sigc::connection conn_handler2;
 };
 
 MyWindow::MyWindow()
 {
   set_title("Exception Handlers");
 
-  Gtk::Box *const box = new Gtk::Box(Gtk::Orientation::VERTICAL, 5);
+  auto box = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::VERTICAL, 5);
   box->set_margin(10);
-  set_child(*Gtk::manage(box));
+  set_child(*box);
 
-  // New exception handlers are inserted at the front of the per-thread
-  // handler list.  Thus, in this example handler2() is executed before
-  // handler1().
-  sigc::connection conn_handler1 = Glib::add_exception_handler(&handler1);
-  sigc::connection conn_handler2 = Glib::add_exception_handler(&handler2);
+  // New exception handlers are inserted at the back of the per-thread
+  // handler list. Thus, in this example handler2() is executed after handler1().
+  conn_handler1 = Glib::add_exception_handler(&handler1);
+  conn_handler2 = Glib::add_exception_handler(&handler2);
 
-  Gtk::Button *const button1 = new MyButton("From virtual method");
-  box->append(*Gtk::manage(button1));
-  Gtk::Button *const button2 = new Gtk::Button("From signal handler");
-  box->append(*Gtk::manage(button2));
-  Gtk::Button *const button3 = new Gtk::Button("Unhandled Glib::Error");
-  box->append(*Gtk::manage(button3));
-  Gtk::Button *const button4 = new Gtk::Button("Unhandled std::exception (crash!)");
-  box->append(*Gtk::manage(button4));
-  Gtk::Button *const button5 = new Gtk::Button("Disconnect handler 1");
-  box->append(*Gtk::manage(button5));
-  Gtk::Button *const button6 = new Gtk::Button("Disconnect handler 2");
-  box->append(*Gtk::manage(button6));
+  box->append(*Gtk::make_managed<MyButton>("From virtual method"));
+  auto button2 = Gtk::make_managed<Gtk::Button>("From signal handler");
+  box->append(*button2);
+  auto button3 = Gtk::make_managed<Gtk::Button>("Unhandled Glib::Error");
+  box->append(*button3);
+  auto button4 = Gtk::make_managed<Gtk::Button>("Unhandled std::exception (crash!)");
+  box->append(*button4);
+  auto button5 = Gtk::make_managed<Gtk::Button>("Disconnect handler 1");
+  box->append(*button5);
+  auto button6 = Gtk::make_managed<Gtk::Button>("Disconnect handler 2");
+  box->append(*button6);
 
   button2->signal_clicked().connect(&global_on_clicked);
   button3->signal_clicked().connect(&global_on_clicked_throw_glib_error);
@@ -124,13 +121,10 @@ MyWindow::MyWindow()
   button5->signal_clicked().connect(sigc::mem_fun(conn_handler1, &sigc::connection::disconnect));
   button6->signal_clicked().connect(sigc::mem_fun(conn_handler2, &sigc::connection::disconnect));
 }
-
 } // anonymous namespace
-
 
 int main(int argc, char** argv)
 {
   auto app = Gtk::Application::create("org.gtkmm.example");
-
   return app->make_window_and_run<MyWindow>(argc, argv);
 }
