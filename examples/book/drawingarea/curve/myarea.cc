3ada0f404cdd339c9b66cbb4c1e53490260e3564
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2025-01-06 15:33:25 +0100

    Drawing Curved Lines: Make some lines dashed
    Fixes #28

--- examples/book/drawingarea/curve/myarea.cc
+++ examples/book/drawingarea/curve/myarea.cc
@@ -16,6 +16,7 @@
 
 #include "myarea.h"
 #include <cairomm/context.h>
+#include <vector>
 
 MyArea::MyArea()
 {
@@ -28,23 +29,33 @@ MyArea::~MyArea()
 
 void MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
 {
-  double x0=0.1, y0=0.5, // start point
-         x1=0.4, y1=0.9,  // control point #1
-         x2=0.6, y2=0.1,  // control point #2
-         x3=0.9, y3=0.5;  // end point
+  const double x0=0.1, y0=0.5, // start point
+               x1=0.4, y1=0.9, // control point #1
+               x2=0.6, y2=0.1, // control point #2
+               x3=0.9, y3=0.5; // end point
+  const std::vector<double> dashes = {0.08, 0.05};
 
   // scale to unit square (0 to 1 width and height)
   cr->scale(width, height);
 
-  cr->set_line_width(0.05);
   // draw curve
+  cr->set_line_width(0.05);
   cr->move_to(x0, y0);
   cr->curve_to(x1, y1, x2, y2, x3, y3);
   cr->stroke();
-  // show control points
-  cr->set_source_rgba(1, 0.2, 0.2, 0.6);
-  cr->move_to(x0, y0);
-  cr->line_to (x1, y1);
+
+  // show first control point (red)
+  cr->set_line_width(0.03);
+  cr->set_dash(dashes, 0.0);
+  cr->set_source_rgba(1.0, 0.2, 0.2, 0.6);
+  cr->set_line_cap(Cairo::Context::LineCap::ROUND);
+  cr->move_to(x1, y1);
+  cr->line_to (x0, y0);
+  cr->stroke();
+
+  // show second control point (blue)
+  cr->set_source_rgba(0.2, 0.2, 1.0, 0.6);
+  cr->set_line_cap(Cairo::Context::LineCap::BUTT);
   cr->move_to(x2, y2);
   cr->line_to (x3, y3);
   cr->stroke();
