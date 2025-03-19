8c51dd62c9b7fd95de05a3d685c3e5398a0581e1
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2025-03-19 11:04:26 +0100

    Add others/webkit example
    Fixes #26 (sort of)

---
+++ examples/others/webkit/examplewindow.cc
@@ -0,0 +1,35 @@
+/* gtkmm example Copyright (C) 2025 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "examplewindow.h"
+#include <webkit/webkit.h>
+
+ExampleWindow::ExampleWindow()
+{
+  set_title("Webkit Frame");
+  set_default_size(600, 400);
+
+  WebKitWebView* wkwv =  WEBKIT_WEB_VIEW(webkit_web_view_new());
+
+  Gtk::Widget* gwkwv = Glib::wrap(GTK_WIDGET(wkwv));
+
+  set_child(*gwkwv);
+
+  webkit_web_view_load_uri(wkwv, "http://www.webkitgtk.org/");
+}
+
+ExampleWindow::~ExampleWindow()
+{
+}
---
+++ examples/others/webkit/examplewindow.h
@@ -0,0 +1,50 @@
+/* gtkmm example Copyright (C) 2025 gtkmm development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GTKMM_EXAMPLEWINDOW_H
+#define GTKMM_EXAMPLEWINDOW_H
+
+// https://gitlab.gnome.org/GNOME/gtkmm-documentation/-/issues/26
+
+// g++ -o app main.cc examplewindow.cc `pkg-config --cflags --libs gtkmm-4.0 webkitgtk-6.0` -std=c++17
+
+// If you get this execution error:
+//   bwrap: setting up uid map: Permission denied
+// see instructions at https://funos.org/solve-bwrap-permission-denied/
+// 1. Add file /etc/apparmor.d/bwrap with contents:
+//      abi <abi/4.0>,
+//      include <tunables/global>
+//
+//      profile bwrap /usr/bin/bwrap flags=(unconfined) {
+//        userns,
+//
+//        # Site-specific additions and overrides. See local/README for details.
+//        include if exists <local/bwrap>
+//      }
+// 2. Restart AppArmor:
+//      sudo systemctl restart apparmor.service
+
+#include <gtkmm.h>
+
+class ExampleWindow : public Gtk::Window
+{
+public:
+  ExampleWindow();
+  ~ExampleWindow() override;
+
+protected:
+};
+
+#endif // GTKMM_EXAMPLEWINDOW_H
