7d9cad19e183957390af5f897209fa11c951e7fd
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2025-05-26 08:52:20 +0200

    Remove obsolete FSF (Free Software Foundation) address in example programs

--- examples/book/buildapp/step5/exampleappwindow.h
+++ examples/book/buildapp/step5/exampleappwindow.h
@@ -1,16 +1,17 @@
 /* gtkmm example Copyright (C) 2016 gtkmm development team
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef GTKMM_EXAMPLEAPPWINDOW_H_
